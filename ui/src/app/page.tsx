b05a5d1591b716bbb71eefef9a5416d1e705bc9b
Камал Курбанов <kamalkurbanov@MacBook-Pro-Kamal.local>
2025-06-28 23:08:31 +0300

    initial ui

---
+++ ui/src/app/page.tsx
@@ -0,0 +1,35 @@
+import Image from 'next/image'
+import { Card, Souvenir } from './components/Card'
+import { GetServerSideProps } from 'next'
+
+type Props = {
+  token: string
+}
+
+// export async function getServerSideProps(): GetServerSideProps<Props> {
+
+//   return {
+//     props: token,
+//   }
+// }
+
+export default async function Page({ token }: Props) {
+  const data = await fetch('http://localhost:3001/api/user/login', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      password: '000111',
+      email: 'base@gmail.ru',
+    }),
+    cache: 'no-store',
+  })
+  const res = await data.json()
+
+  return (
+    <div>
+      <h1>Магазин</h1>
+      token: {res.token}
+      {/* <p>{token}</p> */}
+    </div>
+  )
+}
